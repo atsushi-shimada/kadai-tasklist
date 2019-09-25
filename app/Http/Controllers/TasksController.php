068b43777800e93d88f16c5fa78950ec301a632d
taro.kirameki <taro.kirameki@techacademy.jp>
2019-09-25 23:43:53 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -35,6 +35,10 @@ class TasksController extends Controller
     // postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        $this->validate($request, [
+        'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -65,6 +69,10 @@ class TasksController extends Controller
     // putまたはpatchでtasks/idにアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+        'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->save();
