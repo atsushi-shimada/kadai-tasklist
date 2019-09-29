1f495e1d015783860eab9b87ec7885cc8d269d15
taro.kirameki <taro.kirameki@techacademy.jp>
2019-09-29 23:20:16 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -36,10 +36,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+        'status' => 'required|max:191',   // 追加
         'content' => 'required|max:191',
         ]);
         
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
         
@@ -70,10 +72,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+        'status' => 'required|max:191',   // 追加
         'content' => 'required|max:191',
         ]);
         
         $task = Task::find($id);
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
         
